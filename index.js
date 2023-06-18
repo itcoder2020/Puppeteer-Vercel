2bebca70353cbcf7cd35d2d4c4eca8762e82b4c5
ภานุพงศ์ นิยะกิจ <jkjknm02x@gmail.com>
2023-06-18 13:39:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const app = require("express")();
-
 let chrome = {};
 let puppeteer;
-
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
   chrome = require("chrome-aws-lambda");
   puppeteer = require("puppeteer-core");
@@ -10,7 +8,18 @@ if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
   puppeteer = require("puppeteer");
 }
 
-app.get("/api", async (req, res) => {
+const { URL: URLParser } = require('url');
+app.get("/api/postid", async (req, res) => {
+  const { url } = req.query;
+
+  // Validate URL
+  try {
+    new URLParser(url);
+  } catch (error) {
+    res.status(400).json({ error: 'Invalid URL' });
+    return;
+  }
+
   let options = {};
 
   if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
@@ -25,15 +34,27 @@ app.get("/api", async (req, res) => {
 
   try {
     let browser = await puppeteer.launch(options);
-
     let page = await browser.newPage();
-    await page.goto("https://www.google.com");
-    res.send(await page.title());
+
+    await page.goto(url);
+    const data = await page.evaluate(() => document.querySelector('*').outerHTML);
+    const pattern = /subscription_target_id":"([0-9]+)"/;
+
+    const match = data.match(pattern);
+    if (match) {
+      const targetId = match[1];
+      console.log(targetId);
+      res.status(200).json({code:200, message: targetId });
+    }else{
+      res.status(400).json({code:400, error: 'Id not Found.' });
+    }
+    
   } catch (err) {
     console.error(err);
-    return null;
+    res.status(400).json({code:200, error: err.message });
   }
 });
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server started");
